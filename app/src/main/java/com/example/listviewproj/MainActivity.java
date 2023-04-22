33e836053dbf2819f2b32a0fa05683ce55b682a7
kanticha954 <kanticha.tri@gmail.com>
2023-04-22 13:33:47 +0700

    list

---
+++ app/src/main/java/com/example/listviewproj/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.listviewproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView simpleList;
+    String countryList[] = {"India", "China", "Australia", "Portugol", "America", "NewZealand", "Thailand", "Laos"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.listview);
+        TextView itemSelect = (TextView) findViewById(R.id.item);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //String selectedItem = (String) adapterView.getItemAtPosition(position);
+                Log.d("position", "position"+position);
+                //itemSelect.setText(position);
+                itemSelect.setText("Best Country : "+ countryList[position]);
+
+
+            }
+        });
+
+
+
+
+    }
+}
